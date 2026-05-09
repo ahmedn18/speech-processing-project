074b77aedd5a3f026531854de49f1dc45e663d38
ahmednader <es-ahmed.nader2027@alexu.edu.eg>
2026-05-09 23:19:48 +0300

    Replace LCD header with custom API

--- model_prototype/firmware/include/lcd.h
+++ model_prototype/firmware/include/lcd.h
@@ -3,9 +3,14 @@
 
 #include <stdint.h>
 
-void lcd_init(void);
-void lcd_clear(void);
-void lcd_set_cursor(uint8_t row, uint8_t col);
-void lcd_print(const char *text);
+void LCD_Init(void);
+void LCD_Command(unsigned char cmd);
+void LCD_Char(unsigned char data);
+void LCD_String(char *str);
+void LCD_String_xy(char row, char pos, char *str);
+void LCD_Clear(void);
+void LCD_Create_Char(unsigned char address, unsigned char pattern[]);
+void LCD_Gotoxy(char row, char pos);
+unsigned char LCD_Read_Char(unsigned char address);
 
 #endif
